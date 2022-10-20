9d55f8a9c3f77d5d5157f49779ac9358cb0d21f1
nrgxtra <nrgxtra@hotmail.com>
2022-10-20 09:08:07 +0300

    will try to make async mailing

--- chirper/config/app.php
+++ chirper/config/app.php
@@ -182,6 +182,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
@@ -209,7 +210,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Redis' => Illuminate\Support\Facades\Redis::class,
     ])->toArray(),
 
 ];
