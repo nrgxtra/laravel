29399efbe7856cff6377ab41ae3a040ba470581e
nrgxtra <nrgxtra@hotmail.com>
2022-10-23 20:48:56 +0300

    starting custom auth for barbers

--- final/routes/web.php
+++ final/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
+
+Route::get('auth/login', [MainController::class, 'login'])->name('auth.login');
+Route::get('auth/register', [MainController::class, 'register'])->name('auth.register');
+Route::post('auth/save', [MainController::class, 'save'])->name('auth.save');
+Route::post('auth/logout', [MainController::class, 'logout'])->name('auth.logout');
+Route::post('auth/check', [MainController::class, 'check'])->name('auth.check');
